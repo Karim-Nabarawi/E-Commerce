208bf37dd8d69064899cafb41ff9197cc5aeee01
karim-nabarawi <k.nabarawi@gmail.com>
2021-08-18 00:34:39 +0200

    styled menu items

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import "./styles/App.css";
 //Components
 import HomePage from "./Pages/Home/HomePage";
 
--- src/components/menu-item/MenuItem.js
+++ src/components/menu-item/MenuItem.js
@@ -4,9 +4,10 @@ import "./menu-item.styles.scss";
 
 const MenuItem = ({ data }) => {
   return (
-    <div style={{ backgroundImage: `url(${data.imageUrl})` }} className={`${data.size} menu-item`}>
+    <div className={`${data.size} menu-item`}>
+      <div style={{ backgroundImage: `url(${data.imageUrl})` }} className="background-image"></div>
       <div className="content">
-        <h1 className="title">{data.title}</h1>
+        <h1 className="title">{data.title.toUpperCase()}</h1>
         <span className="subtitle">SHOP NOW</span>
       </div>
     </div>
