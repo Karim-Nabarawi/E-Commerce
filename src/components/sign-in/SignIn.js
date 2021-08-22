e122eb69c87bd57429457d3b0f1964dac3ca14fe
karim-nabarawi <k.nabarawi@gmail.com>
2021-08-22 02:45:15 +0200

    removed popup when clicking sign in with google

--- src/components/sign-in/SignIn.js
+++ src/components/sign-in/SignIn.js
@@ -54,7 +54,7 @@ const SignIn = () => {
         />
         <div className="buttons">
           <CustomButton type="submit">Sign in</CustomButton>
-          <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+          <CustomButton type="button" onClick={signInWithGoogle} isGoogleSignIn>
             Sign in with google
           </CustomButton>
         </div>
