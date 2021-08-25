6e36656dad6597b6ca3b09017103177c948ac6ec
karim-nabarawi <k.nabarawi@gmail.com>
2021-08-25 19:39:12 +0200

    created cart item componenet connected cart dropdown with cart reducer

--- src/components/cart-dropdown/CartDropdown.js
+++ src/components/cart-dropdown/CartDropdown.js
@@ -1,15 +1,22 @@
 import React from "react";
 
+//redux
+import { useSelector } from "react-redux";
+
 //Styling and Animation
 import "./cart-dropdown.styles.scss";
 
 //componenet
 import CustomButton from "../custom-button/CustomButton";
+import CartItem from "../cart-item/CartItem";
 
 const CartDropdown = () => {
+  const { cartItems } = useSelector((state) => state.cart);
+  console.log(cartItems);
   return (
     <div className="cart-dropdown">
-      <div className="cart-items"></div>
+      <div className="cart-items">{cartItems && cartItems.map((item) => <CartItem key={item.id} item={item} />)}</div>
+
       <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
   );
