42a73908b12ca3dbdad63d5ad16d980bd2c757e1
karim-nabarawi <k.nabarawi@gmail.com>
2021-08-27 13:11:01 +0200

    added additem and removeItem to checkout page

--- src/components/checkout-item/CheckoutItem.js
+++ src/components/checkout-item/CheckoutItem.js
@@ -2,7 +2,7 @@ import React from "react";
 
 //redux
 import { useDispatch } from "react-redux";
-import { clearItem } from "../../redux/cart/cart.actions";
+import { clearItem, addItem, removeItem } from "../../redux/cart/cart.actions";
 
 //Styling and Animation
 import "./checkout-item.styles.scss";
@@ -10,18 +10,24 @@ import "./checkout-item.styles.scss";
 const CheckoutItem = ({ cartItem }) => {
   const { imageUrl, name, quantity, price } = cartItem;
   const dispatch = useDispatch();
-  const removeHandler = () => {
-    dispatch(clearItem(cartItem));
-  };
+
   return (
     <div className="checkout-item">
       <div className="image-container">
         <img src={imageUrl} alt="item" />
       </div>
       <span className="name">{name}</span>
-      <span className="quantity">{quantity}</span>
+      <span className="quantity">
+        <div className="arrow" onClick={() => dispatch(removeItem(cartItem))}>
+          &#10094;
+        </div>
+        <div className="value">{quantity}</div>
+        <div className="arrow" onClick={() => dispatch(addItem(cartItem))}>
+          &#10095;
+        </div>
+      </span>
       <span className="price">{price}</span>
-      <div className="remove-button" onClick={removeHandler}>
+      <div className="remove-button" onClick={() => dispatch(clearItem(cartItem))}>
         &#10005;
       </div>
     </div>
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,6 +1,6 @@
 import { CartActionTypes } from "./cart.types";
 
-import { addItemToCart } from "./cart.utils";
+import { addItemToCart, removeItemFromCart } from "./cart.utils";
 
 const initState = {
   hidden: true,
@@ -18,6 +18,11 @@ const cartReducer = (state = initState, action) => {
       return {
         ...state,
         cartItems: addItemToCart(state.cartItems, action.payload),
+      };
+    case CartActionTypes.REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: removeItemFromCart(state.cartItems, action.payload),
       };
     case CartActionTypes.CLEAR_ITEM:
       return {
